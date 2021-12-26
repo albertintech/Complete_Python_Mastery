0ba0ef0cb22de571519019dcd6671174c992639b
Albert Ramos <albertramos.dev@gmail.com>
2021-12-26 16:12:12 -0700

    Update file

--- functions/defining.py
+++ functions/defining.py
@@ -1,1 +1,13 @@
 # pylint: disable=invalid-name, missing-docstring
+
+def greet(first_name, last_name):
+    print(f"Hi there {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+greet("Albert", "Ramos")
+print(get_greeting("Bob"))
