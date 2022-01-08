8458556be2dc0e99f56f47d77b5c58e821ed875d
Albert Ramos <albertramos.dev@gmail.com>
2022-01-08 15:01:48 -0700

    Update file

--- functions/multiply.py
+++ functions/multiply.py
@@ -1,8 +1,11 @@
+"""A program to demonstrate debuggin in VS Code"""
+
 def multiply(*numbers):
+    """Missing docstring"""
     total = 1
     for number in numbers:
         total *= number
-        return total
+    return total
 
 print("Start")
 print(multiply(1,2,3))
